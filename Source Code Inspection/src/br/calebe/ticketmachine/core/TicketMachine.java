df303662460c456cd59222ca637fcea51b2088c8
31403875 <31403875@099397-P31S301.macklabs.br>
2016-03-30 13:10:04 -0300

    Retirada do metodo Iterator da classe TicketMachine

--- Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
@@ -36,9 +36,6 @@ public class TicketMachine {
         return saldo;
     }
 
-    public Iterator<Integer> getTroco() {
-        return null;
-    }
 
     public String imprimir() throws SaldoInsuficienteException {
         if (saldo < valor) {
